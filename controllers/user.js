b02539e8d86432c8ad8937fc9746106c118f657f
kartiknarwal <kartiknarwal38@gmail.com>
2024-06-03 16:12:29 +0530

    added forgot password and google recaptcha

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 import { User } from "../models/User.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import sendMail from "../middlewares/sendMail.js";
+import sendMail, { sendForgotMail } from "../middlewares/sendMail.js";
 import TryCatch from "../middlewares/TryCatch.js";
 export const register =async(req,res)=>{
     try {
@@ -100,5 +100,67 @@ export const loginUser =TryCatch(async(req,res)=>{
 export const myProfile =TryCatch(async(req,res)=>{
     const user =await User.findById(req.user._id);
     res.json({user});
+});
+
+export const forgotPassword = TryCatch(async (req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) 
+        return res.status(404).json({
+            message: "No User with this email",
+        });
+
+    // Generate a token with expiry
+    const token = jwt.sign({ email }, process.env.Forgot_Secret, { expiresIn: '5m' });
+
+    const data = { email, token };
+
+    try {
+        await sendForgotMail("E-learning", data);
+        
+        // Set the token expiration time
+        user.resetPasswordExpire = Date.now() + 5 * 60 * 1000;
+        
+        await user.save();
+
+        res.json({
+            message: "Reset Password Link is sent to your mail",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to send email. Please try again later.",
+        });
+    }
+});
+
+
+export const resetPassword=TryCatch(async(req,res)=>{
+    const decodedData =jwt.verify(req.query.token ,process.env.Forgot_Secret);
+
+    const user =await User.findOne({email:decodedData.email});
+
+    if(!user) return res.status(404).json({
+        message:"No User with this email"
+    });
+
+    if(user.resetPassowrdExpire ===null) return res.status(400).json({
+        message:"Token Expired",
+    });
+    if(user.resetPassowrdExpire <Date.now()){
+        return res.status(400).json({
+            message:"Token Expired",
+        });
+    }
+    const password = await bcrypt.hash(req.body.password,10)
+
+    user.password =password
+
+    user.resetPassowrdExpire =null;
+
+    await user.save()
+    res.json({message :"Password Reset"});
 })
+
    
--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ const schema =new mongoose.Schema({
         ref:"Courses",
     },
 ],
+ resetPassowrdExpire :Date,
 },
 {
     timestamps:true,
